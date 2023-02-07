a296e1a088a0d5f5baf305c533ca97751fe974ef
Armen23023 <mirzoyana805@gmail.com>
2023-02-07 20:18:25 +0400

    WIP

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> arrayList = new ArrayList(sourceList);
+        ArrayList arrayList1 = new ArrayList();
+        if (arrayList.size()<3){
+            return arrayList1;
+        }
+        else {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if ((i+1) % 3 == 0) {
+                    arrayList1.add(arrayList.get(i));
+                    arrayList1.add(arrayList.get(i));
+                }
+            }
+        }
+        return arrayList1;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,25 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList,new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
-    public int compare(String a, String b) {
+    public int compare(String a, String b)   {
+        if ((5*(Integer.parseInt(a)*Integer.parseInt(a))+3)  ==  (5*(Integer.parseInt(b)*Integer.parseInt(b))+3)){
+            if (Integer.parseInt(a)<0){
+                return -1;
+            }
+            else return 1;
+        }
+        else if ((5*(Integer.parseInt(a)*Integer.parseInt(a))+3)  >  (5*(Integer.parseInt(b)*Integer.parseInt(b))+3))
+            return 1;
+        else
+            return -1;
     }
 }
